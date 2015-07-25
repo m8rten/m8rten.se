d3a6df2a38efc1ecaacad8bd5c24288d59e57ba7
marten.hedborg <marten.hedborg@gmail.com>
2015-07-25 17:06:28 +0200

    added api route controll irrigation

--- m8rten-se.groovy
+++ m8rten-se.groovy
@@ -1,15 +1,22 @@
 @GrabResolver(name="netty snapshots", root="http://clinker.netty.io/nexus/content/repositories/snapshots")
 @GrabResolver(name="OJO", root="https://oss.jfrog.org/artifactory/repo")
+@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7' )
 @Grab("io.ratpack:ratpack-groovy:0.9.13")
+@Grab("io.ratpack:ratpack-jackson:0.9.13")   
 import static ratpack.groovy.Groovy.ratpack
 import groovy.json.JsonSlurper
 import groovy.json.JsonBuilder
 import groovy.time.TimeCategory
+import ratpack.jackson.JacksonModule          
+import static ratpack.jackson.Jackson.jsonNode
+import groovyx.net.http.HTTPBuilder
+import static groovyx.net.http.ContentType.JSON
 
 /*
  * Loads configuration
  */
 def mongolabApiKey = System.getenv('MONGOLAB_API_KEY') 
+def vaxthusetAdminKey = System.getenv('VAXTHUSET_ADMIN_KEY') 
 
 /*
  * Updates image every 60 s
@@ -37,6 +44,10 @@ def gifUpdater = Thread.start {
  * Defines handlers
  */
 ratpack {
+    bindings {                                  
+        add new JacksonModule()                   
+    } 
+
     handlers {
 
 		/*
@@ -69,6 +80,20 @@ ratpack {
         get("vaxthuset/api/lowest-temperature") {
             response.send new URL("""https://api.mongolab.com/api/1/databases/vaxthuset/collections/status?f={_id:0}&l=1&s={temperature:1}&q={date:{\$gt:"${oneDayAgo()}"}}&apiKey=$mongolabApiKey""").text
         }
+
+        post("vaxthuset/api/controller") {                             
+            def controller = parse jsonNode()
+            if (controller.key.toString() == "\"$vaxthusetAdminKey\"") {
+                def http = new HTTPBuilder("http://83.68.246.58:50508/api/controller")
+                http.post(body: [key: controller.key.toString(),
+                                time: controller.time.toString().toInteger()],
+                                requestContentType: JSON ) { resp ->
+                                    render resp.status
+                }
+            } else {
+                render "fail"
+            }
+        }   
 
     	/*
     	 * Static stuff
